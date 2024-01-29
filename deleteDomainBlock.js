252bf9649d1b59b557749ebdc53972febeb3f8b0
Kelechukwu Tasie <ktasie@gmail.com>
2024-01-29 14:33:45 +0100

    Fixed bulk delete domain

--- deleteDomainBlock.js
+++ deleteDomainBlock.js
@@ -1,29 +1,52 @@
 import fs from 'fs';
 
-export default function deleteBlockAndCert(domain){
-    const nginxConfig = `/etc/nginx/conf.d/${domain}.conf`;
-    const letsencryptArchive = `/etc/letsencrypt/archive/${domain}`; // Folder;
-    const letsencryptLive = `/etc/letsencrypt/live/${domain}`; // Folder
-    const letsencryptRenewal = `/etc/letsencrypt/renewal/${domain}.conf`
+export default function deleteBlockAndCert(domain) {
+  const nginxConfig = `/etc/nginx/conf.d/${domain}.conf`;
+  const letsencryptArchive = `/etc/letsencrypt/archive/${domain}`; // Folder;
+  const letsencryptLive = `/etc/letsencrypt/live/${domain}`; // Folder
+  const letsencryptRenewal = `/etc/letsencrypt/renewal/${domain}.conf`;
 
+  fs.unlink(nginxConfig, (err) => {
+    if (err) {
+      console.log(`Error deleting nginx config(${domain}.conf) `, err);
+    } else {
+      console.log('Nginx configuration file deleted successfully!');
+    }
+  });
 
-    fs.unlink(nginxConfig, (err)=>{
-        if(err){
-            console.log(`Error deleting nginx config(${domain}.conf) `, err)
-        } else {
-            console.log('Nginx configuration file deleted successfully!')
-        }
-    });
+  fs.unlink(letsencryptRenewal, (err) => {
+    if (err) {
+      console.log(
+        `Error deleting Letsencypt renewal config(${domain}.conf) `,
+        err,
+      );
+    } else {
+      console.log(
+        'Lets encrypt renewal configuration file deleted successfully!',
+      );
+    }
+  });
 
-    fs.unlink(letsencryptRenewal, (err)=>{
-        if(err){
-            console.log(`Error deleting Letsencypt renewal config(${domain}.conf) `, err)
-        } else {
-            console.log('Lets encrypt renewal configuration file deleted successfully!')
-        }
-    });
+  fs.rm(letsencryptArchive, { recursive: true }, (err) => {
+    if (err) {
+      console.log(
+        `Error deleting letsencrypt archive directory (${domain}) `,
+        err,
+      );
+    } else {
+      console.log('Archive directory has been deleted successfully!');
+    }
+  });
+  fs.rm(letsencryptLive, { recursive: true }, (err) => {
+    if (err) {
+      console.log(
+        `Error deleting letsencrypt live directory (${domain}) `,
+        err,
+      );
+    } else {
+      console.log('Live directory has been deleted successfully!');
+    }
+  });
 
-    console.log('Deletion completed.')
-
-
+  console.log('Deletion completed.');
 }
--- singleDomainBlockConfig.js
+++ singleDomainBlockConfig.js
@@ -2,15 +2,15 @@ import fs from 'fs';
 import { execSync } from 'child_process';
 
 export default function singleDomainBlockConfig(choices) {
-    // Replace these values with your actual domain and paths
-    const {domain, ssl} = choices;
-    const email = 'gxcpoperations@galaxybackbone.com.ng';
-    //const nginxConfigPath = `/etc/nginx/conf.d/${domain}.conf`;  
-    const nginxConfigPath = `./${domain}.conf`;
-    let nginxConfig;
+  // Replace these values with your actual domain and paths
+  const { domain, ssl } = choices;
+  const email = 'gxcpoperations@galaxybackbone.com.ng';
+  const nginxConfigPath = `/etc/nginx/conf.d/${domain}.conf`;
+  //const nginxConfigPath = `./${domain}.conf`;
+  let nginxConfig;
 
-    if(ssl === 'N'){
-        nginxConfig = `
+  if (ssl === 'N') {
+    nginxConfig = `
             server {
                 listen 80;
                 listen [::]:80;
@@ -24,9 +24,9 @@ export default function singleDomainBlockConfig(choices) {
                 
             }
         `;
-    } else if(ssl === 'Y'){
-        // Generate your Nginx configuration
-        nginxConfig = `
+  } else if (ssl === 'Y') {
+    // Generate your Nginx configuration
+    nginxConfig = `
         server {
             listen 80;
             listen [::]:80;
@@ -56,29 +56,30 @@ export default function singleDomainBlockConfig(choices) {
         }
         `;
 
-        // Run Certbot to obtain SSL certificate
-        try {
-            execSync(`sudo certbot certonly --nginx -d ${domain} -d www.${domain} --non-interactive --agree-tos -m ${email} --test-cert`);
-            console.log(`SSL certificate obtained successfully for ${domain}!`);
-
-            
-        } catch (err) {
-            console.error(`Error obtaining SSL certificate for ${domain}:`, err.stdout.toString());
-            return;
-        }
-    }
-
-    // Write the Nginx configuration to a file
-    fs.writeFileSync(nginxConfigPath, nginxConfig);
-    
-    // Restart Nginx to apply changes
+    // Run Certbot to obtain SSL certificate
     try {
-        execSync('sudo systemctl restart nginx');
-        console.log('Nginx restarted successfully!');
-        console.log(`Nginx configuration for ${domain} created successfully!`);
+      execSync(
+        `sudo certbot certonly --nginx -d ${domain} -d www.${domain} --non-interactive --agree-tos -m ${email} --test-cert`,
+      );
+      console.log(`SSL certificate obtained successfully for ${domain}!`);
     } catch (err) {
-        console.error('Error restarting Nginx:', err.stdout.toString());
+      console.error(
+        `Error obtaining SSL certificate for ${domain}:`,
+        err.stdout.toString(),
+      );
+      return;
     }
-     
+  }
 
+  // Write the Nginx configuration to a file
+  fs.writeFileSync(nginxConfigPath, nginxConfig);
+
+  // Restart Nginx to apply changes
+  try {
+    execSync('sudo systemctl restart nginx');
+    console.log('Nginx restarted successfully!');
+    console.log(`Nginx configuration for ${domain} created successfully!`);
+  } catch (err) {
+    console.error('Error restarting Nginx:', err.stdout.toString());
+  }
 }
